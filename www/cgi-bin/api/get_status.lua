3992a9fe8abc081e12536ef4a1857c3bbb2a56dc
suzukiin <lucas14112003@gmail.com>
2026-01-08 16:40:05 -0300

    get_status

--- www/cgi-bin/api/get_status.lua
+++ www/cgi-bin/api/get_status.lua
@@ -4,11 +4,13 @@ package.path = package.path .. ";../lib/?.lua"
 local bootstrap = require "bootstrap"
 local vpn = require "vpn"
 local lte = require "lte"
+local json = require "json"
 
 bootstrap.json_header()
 
 local vpn_status, vpn_ip = vpn.get_status()
 local rssi = lte.get_rssi()
+local status_info = json.getInfo()
 
 print(string.format([[
 {
@@ -18,10 +20,17 @@ print(string.format([[
   },
   "lte": {
     "rssi": %s
+  },
+  "status_info": {
+    "localidade": "%s",
+    "cliente": "%s",
+    "id": "%s"
   }
-}
-]],
+}]],
     vpn_status,
     vpn_ip and '"' .. vpn_ip .. '"' or "null",
-    rssi or "null"
+    rssi or "null",
+    status_info.cidade or "",
+    status_info.client or "",
+    status_info.numero_de_serie or "****"
 ))
--- www/cgi-bin/lib/json.lua
+++ www/cgi-bin/lib/json.lua
@@ -1,16 +1,15 @@
 #!/usr/bin/lua
-
 local cjson = require "cjson"
-
--- cabeçalho HTTP (se for CGI)
-print("Content-Type: application/json\n")
-
--- lê o JSON
 local f = io.open("/home/lucas/overlay/www/config.json", "r")
-local content = f:read("*a")
-f:close()
 
--- decodifica
-local data = cjson.decode(content)
+local M = {}
 
-print(cjson.encode(data.info))
+function M.getInfo()
+    local content = f:read("*a")
+    f:close()
+
+    local data = cjson.decode(content)
+    return data.info
+end
+
+return M
